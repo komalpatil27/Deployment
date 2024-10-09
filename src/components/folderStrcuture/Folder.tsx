20c90db939e13e3ab22c71f5657648657fb0933d
komal <komalsubhash2797@gmail.com>
2024-10-09 10:48:12 +0530

    Folder recursion

--- src/components/folderStrcuture/Folder.tsx
+++ src/components/folderStrcuture/Folder.tsx
@@ -1,12 +1,26 @@
-import React from "react";
-import dataStructure from "./structure";
-
 interface FolderProps {
-    dataStructure : {}
+  dataStructure: {
+    name: string;
+    isFolder: boolean;
+    items: any[];
+  };
 }
-const Folder = (props : FolderProps) => {
-    console.log(props , 'data')
-  return <>Folder Check</>;
+const Folder = (props: FolderProps) => {
+  console.log(props, "data");
+  const { dataStructure } = props;
+  return (
+    <>
+      {/* <h4>Folder Structure</h4> */}
+      <div>
+        {dataStructure.isFolder ? `📁 ${dataStructure.name}` :  `🗃️ ${dataStructure.name}`}
+        {dataStructure.isFolder &&
+          dataStructure.items.map((value) => {
+            return (
+            <div style={{padding : '10px'}}><Folder dataStructure={value} /></div>
+          )})}
+      </div>
+    </>
+  );
 };
 
 export default Folder;
